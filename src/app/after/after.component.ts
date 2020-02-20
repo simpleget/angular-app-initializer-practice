1cbb922abfb452595d0e66412bbeeebbe0a08d78
simpleget <simple.yuu@gmail.com>
2020-02-20 17:10:20 +0800

    6 files added

---
+++ src/app/after/after.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-before",
+  templateUrl: "./before.component.html",
+  styleUrls: ["./before.component.scss"]
+})
+export class AfterComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ src/app/before/before.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-before",
+  templateUrl: "./before.component.html",
+  styleUrls: ["./before.component.scss"]
+})
+export class BeforeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
